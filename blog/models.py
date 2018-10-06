6709256a8b15ef205bd6b479c104d44069613d03
grzegorz bednarski <gkbednarski@gmail.com>
2018-10-06 07:52:45 +0200

    compete project

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
   pub_date=models.DateTimeField()
   image=models.ImageField(upload_to='images/')
 
+  def __str__(self):
+      return self.title
+  
+  def summary(self):
+    return self.body[:200]
+
+  def pub_date_short(self):
+    return self.pub_date.strftime('%B %Y')
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
+def allblogs(request):
+  blogView=Blog.objects
+  return render(request, 'blog/allblogs.html',{'blogView':blogView})
 
+def detail(request, blog_id):
+  detailblock=get_object_or_404(Blog, pk=blog_id)
+  return render(request,'blog/detail.html',{'blog':detailblock})
