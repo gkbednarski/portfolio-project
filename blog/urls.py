7a696b50ea15d8db36f769f19f720289d678be0d
gkbed <gkbednarski@gmail.com>
2021-06-04 23:43:10 +0200

    blog funcionality and static folder

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import allblogs
+from .views import allblogs, detail
 
 urlpatterns = [
     path('', allblogs, name='allblogs'),
+    path('<int:blog_id>', detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from blog.models import Blog
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import request
 
 def allblogs(request):
     blogs = Blog.objects
     return render(request, 'blogs/allblogs.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blogs/detail.html', {'blog':detailblog})
